ed65dc5b6ae7ec673712c978d43fd8f78f25a9f2
Łukasz Męderak <lukasz.mederak@gmail.com>
2022-12-05 13:01:57 +0100

    Added user service

---
+++ src/api/services/user.service.ts
@@ -0,0 +1,57 @@
+import { FilterQuery, QueryOptions, UpdateQuery } from 'mongoose';
+
+import { omit } from 'lodash';
+import UserModel from '../models/user.model';
+import { IUserDocument, IUserInput } from '../interfaces/user.interface';
+
+export async function createUser(input: IUserInput) {
+  try {
+    const user = await UserModel.create(input);
+
+    return omit(user.toJSON(), 'password');
+  } catch (e: any) {
+    throw new Error(e);
+  }
+}
+
+export async function validatePassword({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return false;
+  }
+
+  const isValid = await user.comparePassword(password);
+
+  if (!isValid) return false;
+
+  return omit(user.toJSON(), 'password');
+}
+
+export async function getUser(query: FilterQuery<IUserDocument>) {
+  return UserModel.findOne(query).lean();
+}
+
+export async function findAndUpdateUser(
+  query: FilterQuery<IUserDocument>,
+  update: UpdateQuery<IUserDocument>,
+  options: QueryOptions = {}
+) {
+  return UserModel.findOneAndUpdate(query, update, options);
+}
+
+export async function validateEmail(email: string) {
+  const existingUser = await UserModel.findOne({ email });
+
+  if (existingUser) {
+    return true;
+  }
+
+  return false;
+}
