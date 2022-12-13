9f8ed0cd9295a3f1d487c22a44a94c3f2cdbcb5e
Łukasz Męderak <lukasz.mederak@gmail.com>
2022-12-13 14:31:19 +0100

    Added client interfaces

---
+++ src/api/interfaces/client.interfaces.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { IUserDocument } from './user.interface';
+
+export interface IClientInput {
+  user: IUserDocument['_id'];
+  name: string;
+  lastName: string;
+  dateOfBirth: Date;
+  gender: 'male' | 'female';
+  physiologicalState?: 'lack' | 'pregnancy' | 'lactation';
+  email?: string;
+  phoneNumber?: string;
+  street?: string;
+  zipCode?: string;
+  city?: string;
+  notes?: string;
+  expectedBodyWeight?: number;
+  specificAims?: string[];
+  pal: number;
+}
+
+export interface IClientDocument extends IClientInput, mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+}
